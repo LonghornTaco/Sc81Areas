5193c9c59054787912d3171a018a2d0224101051
Jason Wilkerson <jasonmwilkerson@hotmail.com>
2015-10-26 00:20:56 -0500

    Updated code

---
+++ Sc81Areas.Web/Areas/Site1/Site1AreaRegistration.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Sc81Areas.Web.Areas.Site1
+{
+   public class Site1AreaRegistration : AreaRegistration
+   {
+      public override string AreaName { get { return "Site1"; } }
+
+      public override void RegisterArea(AreaRegistrationContext context)
+      {
+
+      }
+   }
+}
---
+++ Sc81Areas.Web/Areas/Site2/Site2AreaRegistration.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Sc81Areas.Web.Areas.Site2
+{
+   public class Site2AreaRegistration : AreaRegistration
+   {
+      public override string AreaName { get { return "Site2"; } }
+
+      public override void RegisterArea(AreaRegistrationContext context)
+      {
+
+      }
+   }
+}
